0b3a348f2388cc9a2527a56c2b85ba14837701f8
Matthew Gamble <git@matthewgamble.net>
2017-02-08 18:36:12 +1100

    Small fixes

--- setup/root/php.sh
+++ setup/root/php.sh
@@ -9,7 +9,6 @@ cat > /etc/php/conf.d/60-adminer-conf.ini <<EOF
 extension=mysqli.so
 extension=pdo_mysql.so
 extension=pdo_pgsql.so
-extension=pdo_pgsql.so
 extension=pdo_sqlite.so
 extension=sqlite3.so
 EOF
